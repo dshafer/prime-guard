1b32642dffe0c2c05dfaabb8aeea3ad576dee7c5
Drew Shafer <drew@toopher.com>
2014-03-19 09:24:22 -0500

    Fix to also work when not logged in

--- extension/prime-guard.js
+++ extension/prime-guard.js
@@ -1,10 +1,23 @@
 (function(window){
-  // is there a "watch from beginning" button?
-  if(document.getElementById("from-beginning-btn") !== null) {
-    // yes, there is.  Remove the option to purchase it.
+  var removeBuyBox = function() {
     var buyBoxen = document.getElementsByClassName("buy-box");
     for(var x=0; x < buyBoxen.length; x++) {
       buyBoxen[x].style.display = "none";
     }
+  }
+
+  // if logged in - is there a "watch from beginning" button?
+  var doRemove = document.getElementById("from-beginning-btn") !== null;
+
+  // if not logged in - is there a "watch for $0.00 with Prime" banner?
+  if (!doRemove) {
+    var dividerTitleDivs = document.getElementsByClassName("divider-title");
+    for(var x=0; x < dividerTitleDivs.length; x++) {
+      doRemove |= dividerTitleDivs[x].innerText === "Watch for $0.00 with Prime";
+    }
+  }
+
+  if (doRemove) {
+    removeBuyBox();
   }
 })(window);
